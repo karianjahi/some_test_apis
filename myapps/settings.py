e60eba3a43b68052411ae6a36b78ccc02efa23d6
karianjahi <kitgaedesigaidi8@gmail.com>
2023-11-19 00:30:02 +0100

    hide variables and downgrade django

--- myapps/settings.py
+++ myapps/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*mlx-v+ph%wvhbvrv8sdli!flgziz3o)=5)+*r-^24frj1c1q'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -93,7 +92,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-DATABASES["default"] = dj_database_url.parse("postgres://epl_user:LtPH2KGRrT0oQaxQ33lHEog6RXFi21zI@dpg-clcjm7rmot1c73dh0700-a.frankfurt-postgres.render.com/epl")
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
